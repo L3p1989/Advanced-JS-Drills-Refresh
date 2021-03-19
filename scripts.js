8754195cfa69c392d9fc6c2dbbf416fe47620436
L3p1989 <phogan110@live.com>
2021-03-18 18:48:11 -0700

    created some logs to separate code. created avg. created function findAve

--- scripts.js
+++ scripts.js
@@ -8,6 +8,23 @@ console.log(name);
 setName();
 //create function setName()
 function setName() {
+  //name is "L3p"
   var name = "L3p";
+  //log name to console
   console.log(name);
+}
+//log "===Line Break===" to console
+console.log("===Line Break===");
+//create avg that uses findAvg(n, n);
+let avg = findAvg(2, 2);
+//log some text and avg to console
+console.log("some text", avg);
+//create function findAvg
+function findAvg(a, b) {
+  //log "===Line Break===" to console
+  console.log("===Line Break===");
+  //answer is a added to be then divided by 2
+  var answer = (a + b) / 2;
+  //returns answer
+  return answer;
 }
