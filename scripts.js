340d631cee126a075581c772fdff6c9ef4d23941
L3p1989 <phogan110@live.com>
2021-03-18 17:40:47 -0700

    called setName, created function setName that creates var name and logs it to console

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+//set name to "L3p"
 name = "L3p";
-let name;
+//create var name
+var name;
+//log name in console
 console.log(name);
+//call function setName()
+setName();
+//create function setName()
+function setName() {
+  var name = "L3p";
+  console.log(name);
+}
